ec9ac260df107c25b1721a2bc97ad851ac67966c
lrloachamin <luisloachamin1128@gmail.com>
2018-11-12 01:33:13 -0500

    upgrade for Luis Loachamin

---
+++ PRIMER PARCIAL/PROJECTSOLF/src/projectsolf/Main.java
@@ -0,0 +1,11 @@
+
+package projectsolf;
+
+public class Main {
+       public static void main(String[] args) {
+   Store store1 = new Store("panaderia","Quito");
+   store1.showStoreData();
+    
+}
+    
+}
